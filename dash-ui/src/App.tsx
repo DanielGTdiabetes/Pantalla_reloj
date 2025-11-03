578e121b5468f5699b4d978641b1fc2238018e92
DanielGTdiabetes <danieltellols@gmail.com>
2025-11-03 19:17:50 +0100

    eliminar banner modo compatibildiad

--- dash-ui/src/App.tsx
+++ dash-ui/src/App.tsx
@@ -4,7 +4,6 @@ import { Route, Routes } from "react-router-dom";
 import GeoScopeMap from "./components/GeoScope/GeoScopeMap";
 import { MapFrame } from "./components/MapFrame";
 import { RightPanel } from "./components/RightPanel";
-import SwiftShaderNotice from "./components/SwiftShaderNotice";
 import { ConfigPage } from "./pages/ConfigPage";
 import DiagnosticsAutoPan from "./pages/DiagnosticsAutoPan";
 
@@ -23,14 +22,11 @@ const DashboardShell: React.FC = () => {
 
 const App: React.FC = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<DashboardShell />} />
-        <Route path="/config" element={<ConfigPage />} />
-        <Route path="/diagnostics/auto-pan" element={<DiagnosticsAutoPan />} />
-      </Routes>
-      <SwiftShaderNotice />
-    </>
+    <Routes>
+      <Route path="/" element={<DashboardShell />} />
+      <Route path="/config" element={<ConfigPage />} />
+      <Route path="/diagnostics/auto-pan" element={<DiagnosticsAutoPan />} />
+    </Routes>
   );
 };
 
